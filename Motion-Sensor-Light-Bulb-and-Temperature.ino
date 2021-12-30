489fdae238e5190cb2648c0ba1235814f7518c89
acilione <41536587+acilione@users.noreply.github.com>
2021-12-30 15:16:36 +0100

    formatted

--- Motion-Sensor-Light-Bulb-and-Temperature.ino
+++ Motion-Sensor-Light-Bulb-and-Temperature.ino
@@ -8,7 +8,7 @@ int min = 1023;
 int max = 0;
 int light;
 int turnedOnTime;
-int red_light_pin= 11;
+int red_light_pin = 11;
 int blue_light_pin = 10;
 int green_light_pin = 9;
 
@@ -56,37 +56,37 @@ void setup()
 
 void loop()
 {
-  n = analogRead(A0);
-  int light = analogRead(A1);
-  float volt = (n/1023.0)*5;
-  float temp = (volt - 0.5)*100; 
-  float ambientLightLevel = map(light, min, max, 0 ,100);
-  Serial.println(ambientLightLevel);
-  if(moving)
-  {
-     if (ambientLightLevel < 60)
-     	digitalWrite(pinRelay, HIGH);
-     lcd.begin(16, 2);
-     lcd.setCursor(0, 0);
-  	 lcd.print("Temperatura:");
-  	 lcd.setCursor(0,1);
-  	 lcd.print(temp);
-     if (temp < 0)
-     	RGB_color(0, 0, 255);
-     else if (temp > 0 && temp < 30)
-        RGB_color(255, 255, 0);
-     else
-       	RGB_color(255, 0, 0);
-     delay(5000);
-   }
-   else
-   {
-   	 digitalWrite(pinRelay, LOW);
-     lcd.noDisplay();
-     digitalWrite(red_light_pin, LOW);
-     digitalWrite(blue_light_pin, LOW);
-     digitalWrite(green_light_pin, LOW);
-   }
-  int val = digitalRead(3);
-  Serial.println(val);
+	n = analogRead(A0);
+	int light = analogRead(A1);
+	float volt = (n / 1023.0) * 5;
+	float temp = (volt - 0.5) * 100; 
+	float ambientLightLevel = map(light, min, max, 0 ,100);
+	Serial.println(ambientLightLevel);
+	if (moving)
+	{
+		if (ambientLightLevel < 60)
+			digitalWrite(pinRelay, HIGH);
+		lcd.begin(16, 2);
+		lcd.setCursor(0, 0);
+		lcd.print("Temperatura:");
+		lcd.setCursor(0,1);
+		lcd.print(temp);
+		if (temp < 0)
+			RGB_color(0, 0, 255);
+		else if (temp > 0 && temp < 30)
+			RGB_color(255, 255, 0);
+		else
+		  	RGB_color(255, 0, 0);
+		delay(5000);
+	}
+	else
+	{
+	 	digitalWrite(pinRelay, LOW);
+	   	lcd.noDisplay();
+	   	digitalWrite(red_light_pin, LOW);
+	   	digitalWrite(blue_light_pin, LOW);
+	   	digitalWrite(green_light_pin, LOW);
+	}
+	int val = digitalRead(3);
+	Serial.println(val);
 }
